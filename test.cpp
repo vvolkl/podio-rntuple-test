5e80a729baa4367417879bbf16c18f2b4478e8da
Valentin Volkl <valentin.volkl@cern.ch>
2021-10-27 15:21:37 +0200

    up

--- test.cpp
+++ test.cpp
@@ -19,82 +19,62 @@ int main() {
 
     {
       auto model = RNTupleModel::Create();
+      // start with a std vector as per examples
       auto fldVpx = model->MakeField<std::vector<float>>("vpx");
-      //auto part = model->MakeField<std::vector<edm4hep::MCParticleData>>("part");
-
-       auto field = RFieldBase::Create("part",  "vector<edm4hep::MCParticleData>").Unwrap();
-       auto vec = new edm4hep::MCParticleCollection();
-       auto buffers = vec->getBuffers();
-
-       std::vector<edm4hep::MCParticleData> **v = new std::vector<edm4hep::MCParticleData> *();
-       *v = buffers.dataAsVector<edm4hep::MCParticleData>();
-       void* vv =  (void*) *static_cast<std::vector<float>**>(buffers.data);
-       //model->GetDefaultEntry()->CaptureValue(field->CaptureValue(*v));
-       model->GetDefaultEntry()->CaptureValue(field->CaptureValue(vv));
-       model->GetFieldZero()->Attach(std::move(field));
+      // an example podio collection to be persistified
+      auto vec = new edm4hep::MCParticleCollection();
+      auto buffers = vec->getBuffers();
+      // create field for a podio collection
+      // in the same conditions as in the event store: only based on a string with the typename and the pointer to the buffer
+      auto field = RFieldBase::Create("part",  "vector<edm4hep::MCParticleData>").Unwrap();
+      // what I cast buffers.data to here does not seme to matter, but it seems I have to cast it to something?
+      void* vv =  (void*) *static_cast<std::vector<float>**>(buffers.data); 
+      model->GetDefaultEntry()->CaptureValue(field->CaptureValue(vv));
+      model->GetFieldZero()->Attach(std::move(field));
+      
       auto ntuple = RNTupleWriter::Recreate(std::move(model), "F", "tmp.root");
+
       for (int i = 0; i < 100; ++i) { //event loop
         fldVpx->clear();
         vec->clear();
         for (int j = 0; j < 1000; ++j) {
+          // set some dummy values for later checks
           float px, py, pz;
           gRandom->Rannor(px, py);
           fldVpx->emplace_back(px);
           edm4hep::MCParticle p;
           p.setCharge(123);
+          p.setMomentum({px, py, 234.});
           vec->push_back(p);
-          
-
-          }
-
+        }
         vec->prepareForWrite();
         std::cout<< vec->size() << "\t" << vec->getBuffers().dataAsVector<edm4hep::MCParticleData>()->size() << std::endl;
         ntuple->Fill();
+        std::cout << "end of event " << i << std::endl;
         }
     }
 
+    // part two: read back the collection from file
     {
-
       auto model = RNTupleModel::Create();
-      //auto fldVpx = model->MakeField<std::vector<float>>("vpx");
-      //auto part = model->MakeField<std::vector<edm4hep::MCParticleData>>("part");
-      
-
-       auto field = RFieldBase::Create("part",  "vector<edm4hep::MCParticleData>").Unwrap();
-       
-
-       auto rv = TClass::GetClass("vector<edm4hep::MCParticleData>");
-       auto rvc = TClass::GetClass("edm4hep::MCParticleCollection");
-
-       auto vec = static_cast<podio::CollectionBase*>(rvc->New()); // new edm4hep::MCParticleCollection();
-       auto buffers = vec->getBuffers();
-       //auto rvc = rv->New();
-             auto collection = 
-                     static_cast<podio::CollectionBase*>(rvc->New());
-       auto collBuffers = collection->getBuffers();
-       collBuffers.data = rv->New();
-
-       //std::vector<edm4hep::MCParticleData> **v = new std::vector<edm4hep::MCParticleData> *();
-       //*v = buffers.dataAsVector<edm4hep::MCParticleData>();
-       void* vv =  (void*) *static_cast<std::vector<float>**>(buffers.data);
-       model->GetDefaultEntry()->CaptureValue(field->CaptureValue(vv));
-       model->GetFieldZero()->Attach(std::move(field));
-
-
-
-
+      // as per open lhcb example
+      auto field = RFieldBase::Create("part",  "vector<edm4hep::MCParticleData>").Unwrap();
+      model->AddField(std::move(field));
+      void *fieldDataPtr = model->GetDefaultEntry()->GetValue("part").GetRawPtr();
       auto ntuple = RNTupleReader::Open(std::move(model), "F", "tmp.root");
+      // can now use fielDataPtr to set the buffers of a new collection, tbd.
+      // debug outputs to check correctness
+      // this amounts to user code, so we can cast explicitly
       ntuple->Show(41);
       for (auto entryId : *ntuple) {
         ntuple->LoadEntry(entryId);
-        //std::cout << part->at(0).vertex.x << std::endl;
-        //std::cout << (*v)->size() << std::endl;
-
-
+        std::cout << (static_cast<std::vector<edm4hep::MCParticleData>*>(fieldDataPtr))->size() << std::endl;
       }
 
 
     }
+
+
 
 
 
